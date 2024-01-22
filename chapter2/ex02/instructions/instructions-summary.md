076118e1c9c2fab91d67be240a7ed1769e4adb7f
Career Ed Service Account <144473079+careeredsrvusr@users.noreply.github.com>
2024-01-22 17:00:40 -0500

    Initial commit

---
+++ chapter2/ex02/instructions/instructions-summary.md
@@ -0,0 +1,5 @@
+## Your Tasks
+
+In this lab, you declare and initialize constants in a C++ program. The program, which is in a file named **NewAge2.cpp**, calculates your age in the year 2050.
+
+## Instructions
---
+++ chapter7/ex01/student/Flowers.cpp
@@ -0,0 +1,36 @@
+Flowers.cpp
+Data Files
+·
+Updated Nov 18, 2022 by Julie
+
+
+
+Open
+Download
+
+
+Share
+// Flowers.cpp - This program reads names of flowers and whether they are grown in shade or sun from an input 
+// file and prints the information to the user's screen. 
+// Input:  flowers.dat.
+// Output: Names of flowers and the words sun or shade.
+
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+   // Declare variables here
+   		
+   // Open input file
+   
+   
+   // Write while loop that reads records from file.
+   fin >> flowerName;
+   
+      // Print flower name
+      
+   fin.close(); 	
+   return 0;
+} 
